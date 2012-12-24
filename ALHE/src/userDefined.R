946fd5522be0812f323d6218921adc9f08c8990b
przemo509@gmail.com <przemo509@gmail.com@7704d3cd-4f6d-5273-928c-ff65015784f8>
2012-12-24 17:22:45 +0000

    Wydzielenie funckji do osobnego pliku

--- ALHE/src/userDefined.R
+++ ALHE/src/userDefined.R
@@ -4,4 +4,10 @@
 ###############################################################################
 
 
-
+# funkcja definiujaca zakresy mozliwych wartosci dla kazdego wymiaru
+generateLimits = function() {
+    limitDim1 = list(left=-3, right=4);
+    limitDim2 = list(left=-2, right=5);
+    limits = list(limitDim1, limitDim2);
+    return(limits);
+}
